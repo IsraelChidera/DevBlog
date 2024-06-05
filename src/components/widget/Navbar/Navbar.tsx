3fa23556dba4972a39dd79cf6f43b19ce1e6fb20
IsraelChidera <israelchidera54@gmail.com>
2024-06-05 10:50:08 +0100

    Integrated tailwind css and shadcn

---
+++ src/components/widget/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+
+
+const Navbar = () => {
+
+  const logo: string = import.meta.env.BLOG_LOGO;
+  const title: string = import.meta.env.BLOG_TITLE;
+
+  console.log("title", title);
+  console.log("logo", logo);
+  
+  return (
+    <nav className='flex items-center justify-between'>
+      <div>
+        <img src={logo} alt="blog logo" />
+      </div>
+
+      <button>
+        here
+      </button>
+    </nav>
+  )
+}
+
+export default Navbar
