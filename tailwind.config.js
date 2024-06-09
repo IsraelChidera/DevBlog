9df51739ff03b057263032b49adb2c54b3bed576
IsraelChidera <israelchidera54@gmail.com>
2024-06-09 17:07:27 +0100

    fix: typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         "2xl": "1400px",
       },
     },
-    extend: {
+    extend: {     
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -73,5 +73,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('@tailwindcss/typography'),
+  ],
 }
